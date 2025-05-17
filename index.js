c80ad493edeba307d674c10c50641b74681243e3
MD SIFAT AHAMED~ <mdsifat978@gmail.com>
2025-05-17 11:43:00 +0600

    inital setup and mongoDb database connected

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@sifatahamed0.ruxo7fd.mongodb.net/?retryWrites=true&w=majority&appName=sifatAhamed0`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/',(req,res)=>{
+    res.send("Coffe store server is Runing");
+})
+app.listen(port,()=>{
+    console.log(`Coffe Store server is Runing Port: ${port}`);
+})
