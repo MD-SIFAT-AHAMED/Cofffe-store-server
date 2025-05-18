615a32f3e46e860353db74787c509f4e4d8e20cb
MD SIFAT AHAMED~ <mdsifat978@gmail.com>
2025-05-18 20:19:00 +0600

    product post and product get functionality complete

--- index.js
+++ index.js
@@ -25,6 +25,21 @@ async function run() {
 
     await client.connect();
 
+    const coffeesCollection = client.db("coffeesDb").collection("coffees");
+
+    app.get("/coffees",async(req,res)=>{
+      const cursor = coffeesCollection.find();
+      const result =await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/coffees',async(req,res)=>{
+      const newCoffee = req.body;
+      const result = await coffeesCollection.insertOne(newCoffee);
+      res.send(result);
+    })
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
